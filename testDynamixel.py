243c766b34d87e5c444903e8558c5a2b8aaec4fd
Camilo Camacho <camilo.im93@gmail.com>
2019-01-30 19:51:12 -0500

    Added TX and RX, and others

--- testDynamixel.py
+++ testDynamixel.py
@@ -3,14 +3,31 @@ from threading import Thread, Timer
 import time
 import numpy as np
 import dynamixel_sdk
+import json
+
+import signal
+import sys
 
 class IODynamixel:
 	"Library to control Dynamixel motors"
 
-	def __init__(self, motors, freq=50, port='/dev/ttyUSB0', baudrate=1000000, protocol=1.0, debug=True):
-		self.motors = motors
-		for motor in motors:
-			motors[motor]['currentPosition'] = float('nan') 
+	ADDR_MX_TORQUE_ENABLE = 24
+	LEN_MX_TORQUE_ENABLE = 1
+	ADDR_MX_GOALPOS_ENABLE = 30
+	LEN_MX_GOALPOS_ENABLE = 2
+	ADDR_MX_PRESENT_POSITION = 36
+
+	def __init__(self, json_file, freq=50, port='/dev/ttyUSB0', baudrate=1000000, protocol=1.0, debug=True):
+		with open(json_file) as f:
+			self.motors = json.load(f)
+		for motor in self.motors:
+			self.motors[motor]['currentPosition'] = int(-1)
+			self.motors[motor]['motorAngle'] = float('nan')
+			self.motors[motor]['robotAngle'] = float('nan')
+			self.motors[motor]['torqueEnable'] = int(0)
+			self.motors[motor]['goalPosition'] = int(-1)
+			self.motors[motor]['motorGoal'] = float('nan')
+			self.motors[motor]['robotGoal'] = self.motors[motor]['initPos']
 		self.freq = freq
 		self.period = 1/self.freq
 		self.port = port
@@ -18,21 +35,126 @@ class IODynamixel:
 		self.protocol = protocol
 		self.debug = debug
 		self.running = False
+		self.threadOn = False
+		self.testTime = time.time()
+		self.sendTorque = False
 		# Initialize PortHandler instance
 		self.portHandler = dynamixel_sdk.PortHandler(self.port)
 		# Initialize PacketHandler instance
 		self.packetHandler = dynamixel_sdk.PacketHandler(self.protocol)
-		# Initialize GroupSyncWrite instance
-		#self.groupSyncWrite = dynamixel_sdk.GroupSyncWrite(self.portHandler, self.packetHandler, ADDR_MX_GOAL_POSITION, LEN_MX_GOAL_POSITION)
+
 		if not self.openPort():
 			print("Failed opening port")
 		if not self.ping():
 			print("Failed doing ping")
 
+		# Initialize GroupSyncWrite instances
+		self.groupSyncTorque = dynamixel_sdk.GroupSyncWrite(\
+			self.portHandler, self.packetHandler, \
+			IODynamixel.ADDR_MX_TORQUE_ENABLE, IODynamixel.LEN_MX_TORQUE_ENABLE)
+		self.groupSyncGoalPos = dynamixel_sdk.GroupSyncWrite(\
+			self.portHandler, self.packetHandler, \
+			IODynamixel.ADDR_MX_GOALPOS_ENABLE, IODynamixel.LEN_MX_GOALPOS_ENABLE)
+		for motor in self.motors:
+			self.groupSyncTorque.addParam(self.motors[motor]['id'], [0])
+		self.groupSyncTorque.txPacket()
+			
+
+		#self.syncWriteTorque()
+		#self.groupSyncWrite = dynamixel_sdk.GroupSyncWrite(self.portHandler, self.packetHandler, ADDR_MX_GOAL_POSITION, LEN_MX_GOAL_POSITION)
+
+	def syncWriteTorque(self):
+		for motor in self.motors:
+			self.groupSyncTorque.changeParam(self.motors[motor]['id'], \
+				[self.motors[motor]['torqueEnable']])
+		self.groupSyncTorque.txPacket()
+
+	def syncWriteGoalPos(self):
+		self.groupSyncGoalPos.clearParam()
+		for motor in self.motors:
+			if self.motors[motor]['torqueEnable']:
+				param_goal_position = \
+					[dynamixel_sdk.DXL_LOBYTE(dynamixel_sdk.DXL_LOWORD(self.motors[motor]['goalPosition'])), \
+					dynamixel_sdk.DXL_HIBYTE(dynamixel_sdk.DXL_LOWORD(self.motors[motor]['goalPosition'])) \
+					]
+				self.groupSyncGoalPos.addParam(self.motors[motor]['id'], param_goal_position)
+		self.groupSyncGoalPos.txPacket()
+
+	def enableTorque(self, names):
+		for name in names:
+			self.motors[name]['torqueEnable'] = int(1)
+		self.sendTorque = True
+
+	def disableTorque(self, names):
+		for name in names:
+			self.motors[name]['torqueEnable'] = int(0)
+		self.sendTorque = True
+
 	def txrx(self):
-		print('Hola')
+		while(self.threadOn):
+			pass
+		self.threadOn = True
 		if self.running:
 			Timer(self.period, self.txrx).start()
+		# print(1/(time.time() - self.testTime))
+		self.testTime = time.time()
+		# TX
+		for motor in self.motors:
+			if self.motors[motor]['invert']:
+				self.motors[motor]['motorGoal'] = \
+					self.motors[motor]['offset'] - self.motors[motor]['robotGoal']
+			else:
+				self.motors[motor]['motorGoal'] = \
+					self.motors[motor]['offset'] + self.motors[motor]['robotGoal']
+
+			if self.motors[motor]['type'] == "MX-28":
+				self.motors[motor]['goalPosition'] = \
+					int((self.motors[motor]['motorGoal']+180)*4096.0/360.0)
+				if self.motors[motor]['goalPosition'] < 0:
+					self.motors[motor]['goalPosition'] = 0
+				elif self.motors[motor]['goalPosition'] > 4095:
+					self.motors[motor]['goalPosition'] = 4095
+			elif self.motors[motor]['type'] == "RX-28" or self.motors[motor]['type'] == "AX-12":
+				self.motors[motor]['goalPosition'] = \
+					int((self.motors[motor]['motorGoal']+150)*1024.0/300.0)
+				if self.motors[motor]['goalPosition'] < 0:
+					self.motors[motor]['goalPosition'] = 0
+				elif self.motors[motor]['goalPosition'] > 1023:
+					self.motors[motor]['goalPosition'] = 1023
+		self.syncWriteGoalPos()
+		if self.sendTorque:
+			self.syncWriteTorque()
+			self.sendTorque = False
+		# RX
+		for name in self.motors:
+			DXL_ID = self.motors[name]['id']
+			dxl_present_position, dxl_comm_result, dxl_error = \
+				self.packetHandler.read2ByteTxRx(self.portHandler, DXL_ID, IODynamixel.ADDR_MX_PRESENT_POSITION)
+			if dxl_comm_result != dynamixel_sdk.COMM_SUCCESS:
+				self.motors[name]['currentPosition'] = float('nan')
+				self.motors[name]['motorAngle'] = float('nan')
+				# print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+			elif dxl_error != 0:
+				self.motors[name]['currentPosition'] = float('nan')
+				self.motors[name]['motorAngle'] = float('nan')
+				# print("%s" % packetHandler.getRxPacketError(dxl_error))
+			else: 
+				self.motors[name]['currentPosition'] = float(dxl_present_position)
+
+				if self.motors[name]['type'] == "MX-28":
+					self.motors[name]['motorAngle'] = float(dxl_present_position)*360.0/4096.0 - 180.0
+				elif self.motors[name]['type'] == "RX-28" or self.motors[name]['type'] == "AX-12":
+					self.motors[name]['motorAngle'] = float(dxl_present_position)*300.0/1024.0 - 150.0
+				else:
+					self.motors[name]['motorAngle'] = float('nan')
+
+				if self.motors[name]['invert']:
+					self.motors[name]['robotAngle'] = -(self.motors[name]['motorAngle'] - self.motors[name]['offset'])
+				else:
+					self.motors[name]['robotAngle'] = self.motors[name]['motorAngle'] - self.motors[name]['offset']
+			# print("ID="+str(DXL_ID)+" POS="+str(dxl_present_position))
+		self.threadOn = False
+		
 
 	def start(self):
 		self.running = True
@@ -43,7 +165,7 @@ class IODynamixel:
 
 	def ping(self):
 		for name in self.motors:
-			DXL_ID = motors[name]['id']
+			DXL_ID = self.motors[name]['id']
 			dxl_model_number, dxl_comm_result, dxl_error = \
 				self.packetHandler.ping(self.portHandler, DXL_ID)
 			if dxl_comm_result != dynamixel_sdk.COMM_SUCCESS:
@@ -73,15 +195,17 @@ class IODynamixel:
 			print("Failed opening the port")
 			return False
 
-motors = {
-		'abs_z':{'id':33, 'type':'MX-28'},
-		'bust_y':{'id':34, 'type':'RX-28-28'},
-		}
-# motors = [33, 34, 35, 36, 37, 41, 42, 43, 44, 51, 52, 53, 54]
+def signal_handler(sig, frame):
+        dxl.stop()
+        sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+dxl = IODynamixel(json_file="motor_definitions.json", freq=40)
+
+#while(True):
+#	dxl.txrx()
+
+dxl.start()
 
 # echo 1 > /sys/bus/usb-serial/devices/ttyUSB0/latency_timer
-
-dxl = IODynamixel(motors=motors, freq=10)
-dxl.start()
-time.sleep(5)
-dxl.stop()
--- trash/test_TTL_RS485.py
+++ trash/test_TTL_RS485.py
@@ -28,11 +28,13 @@ BAUDRATE = 1000000
 DEVICENAME = '/dev/ttyUSB0'
 TORQUE_ENABLE               = 1                 # Value for enabling the torque
 TORQUE_DISABLE              = 0                 # Value for disabling the torque
-DXL_MINIMUM_POSITION_VALUE  = 0		           	# Dynamixel will rotate between this value
-DXL_MAXIMUM_POSITION_VALUE  = 1000         	    # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
+DXL_MINIMUM_POSITION_VALUE  = 2300		       	# Dynamixel will rotate between this value
+DXL_MAXIMUM_POSITION_VALUE  = 2500         	    # and this value (note that the Dynamixel would not move when the position value is out of movable range. Check e-manual about the range of the Dynamixel you use.)
 DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
-DXL_IDS = [33, 34, 35, 36, 37, 41, 42, 43, 44, 51, 52, 53, 54]
+DXL_IDS = [54, 44]
 dxl_goal_position = [DXL_MINIMUM_POSITION_VALUE, DXL_MAXIMUM_POSITION_VALUE]
+
+pos = [[2500, 420], [2300, 480]]
 
 # Control table address
 # Control table address is different in Dynamixel model
@@ -41,7 +43,7 @@ ADDR_MX_GOAL_POSITION      = 30
 ADDR_MX_PRESENT_POSITION   = 36
 
 # Data Byte Length
-LEN_MX_GOAL_POSITION       = 4
+LEN_MX_GOAL_POSITION       = 2
 LEN_MX_PRESENT_POSITION    = 4
 
 index = 0
@@ -76,45 +78,6 @@ for DXL_ID in DXL_IDS:
 	else:
 		print("[ID:%03d] ping Succeeded. Dynamixel model number : %d" % (DXL_ID, dxl_model_number))
 
-
-# Disable Motors Torque
-for DXL_ID in DXL_IDS:
-	dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_DISABLE)
-	if dxl_comm_result != COMM_SUCCESS:
-		print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-	elif dxl_error != 0:
-		print("%s" % packetHandler.getRxPacketError(dxl_error))
-	else:
-		print("Dynamixel#%d has been successfully connected (Torque OFF)" % DXL_ID)
-
-while(True):
-	start = time.time()
-	for DXL_ID in DXL_IDS:
-		dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION)
-		if dxl_comm_result != COMM_SUCCESS:
-			print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-		elif dxl_error != 0:
-			print("%s" % packetHandler.getRxPacketError(dxl_error))
-	end = time.time()
-	print(1/(end - start))
-
-exit()
-
-while(True):
-	for DXL_ID in DXL_IDS:
-		dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read2ByteTxRx(portHandler, DXL_ID, ADDR_MX_PRESENT_POSITION)
-		if dxl_comm_result != COMM_SUCCESS:
-			print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
-		elif dxl_error != 0:
-			print("%s" % packetHandler.getRxPacketError(dxl_error))
-		print("ID="+str(DXL_ID)+" POS="+str(dxl_present_position))
-	time.sleep(0.1)
-	print('\n')
-
-exit()
-
-
-
 # Enable Motors Torque
 for DXL_ID in DXL_IDS:
 	dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID, ADDR_MX_TORQUE_ENABLE, TORQUE_ENABLE)
@@ -125,17 +88,23 @@ for DXL_ID in DXL_IDS:
 	else:
 		print("Dynamixel#%d has been successfully connected (Torque ON)" % DXL_ID)
 
+for i, DXL_ID in enumerate(DXL_IDS):
+		dxl_addparam_result = groupSyncWrite.addParam(DXL_ID, [0,0])
+		if dxl_addparam_result != True:
+			print("[ID:%03d] groupSyncWrite addparam failed" % DXL_ID)
+			quit()
+groupSyncWrite.txPacket()
+
 for i in range(0,10):
 	# Allocate goal position value into byte array
-	param_goal_position = \
-			[DXL_LOBYTE(DXL_LOWORD(dxl_goal_position[index])), \
-			DXL_HIBYTE(DXL_LOWORD(dxl_goal_position[index])), \
-			DXL_LOBYTE(DXL_HIWORD(dxl_goal_position[index])), \
-			DXL_HIBYTE(DXL_HIWORD(dxl_goal_position[index]))]
 
 	# Add Dynamixel Motors goal position value to the Syncwrite parameter storage
-	for DXL_ID in DXL_IDS:
-		dxl_addparam_result = groupSyncWrite.addParam(DXL_ID, param_goal_position)
+	for i, DXL_ID in enumerate(DXL_IDS):
+		param_goal_position = \
+			[DXL_LOBYTE(DXL_LOWORD(pos[index][i])), \
+			DXL_HIBYTE(DXL_LOWORD(pos[index][i])) \
+			]
+		dxl_addparam_result = groupSyncWrite.changeParam(DXL_ID, param_goal_position)
 		if dxl_addparam_result != True:
 			print("[ID:%03d] groupSyncWrite addparam failed" % DXL_ID)
 			quit()
@@ -146,7 +115,7 @@ for i in range(0,10):
 		print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
 
 	# Clear syncwrite parameter storage
-	groupSyncWrite.clearParam()
+	# groupSyncWrite.clearParam()
 
 	# Read Dynamixel present position
 	for DXL_ID in DXL_IDS:
